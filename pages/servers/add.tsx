259a52cb78b69cf894cbf48febacacb87017580f
Shaun Anderson <shaun.anderson@outlook.com>
2021-12-27 15:10:52 +1100

    Added date picker and updated form

--- pages/servers/add.tsx
+++ pages/servers/add.tsx
@@ -1,13 +1,28 @@
-import type { ReactElement } from "react";
+import { ReactElement, forwardRef } from "react";
 import Layout from "../../components/layout";
 import Sidebar from "../../components/sidebar";
-import { Button, Title, Grid, Col, TextInput } from "@mantine/core";
+import {
+  Button,
+  Title,
+  Grid,
+  Col,
+  TextInput,
+  Text,
+  Group,
+  Select,
+  Avatar,
+  Loader,
+  Card,
+  Divider,
+  Center,
+} from "@mantine/core";
 import { ReactTable } from "../../components/table";
 import { useModals } from "@mantine/modals";
 import { useForm, useForceUpdate } from "@mantine/hooks";
 import Link from "next/link";
 import Header from "../../components/header";
 import Breadcrumbs from "../../components/breadcrumb";
+import { DatePicker } from "@mantine/dates";
 
 type Server = {
   id: number;
@@ -110,11 +125,29 @@ export default function About() {
     });
   };
 
+  const SelectItem = forwardRef(
+    ({ image, label, description, ...others }, ref) => (
+      <div ref={ref} {...others}>
+        <Group noWrap>
+          <Avatar src={image} />
+
+          <div>
+            <Text>{label}</Text>
+            <Text size="xs" color="dimmed">
+              {description}
+            </Text>
+          </div>
+        </Group>
+      </div>
+    )
+  );
+
   return (
     <section>
-      <Breadcrumbs />
       <Header
-        title="Add A Server"
+        title="Add Server"
+        showBreadcumbs
+        subTitle="Add a new server to the inventory"
         rightArea={
           <Link href="../../servers">
             <Button component="a" variant="light" color="gray">
@@ -124,16 +157,115 @@ export default function About() {
           </Link>
         }
       />
+      <Center>
+        <div style={{ maxWidth: 800, width: "100%", position: "relative" }}>
+          <Card>
+            <form onSubmit={form.onSubmit((values) => add())}>
+              <Grid>
+                <Col span={3}>
+                  <TextInput
+                    required
+                    label="Asset Number"
+                    {...form.getInputProps("assetNumber")}
+                  />
+                </Col>
+                <Col span={3}>
+                  <TextInput
+                    required
+                    label="Location"
+                    {...form.getInputProps("assetNumber")}
+                  />
+                </Col>
+                <Col span={6}>
+                  {" "}
+                  <DatePicker
+                    placeholder="Purchased date"
+                    label="Purchased Date"
+                    required
+                  />
+                </Col>
+              </Grid>
 
-      <form onSubmit={form.onSubmit((values) => add())}>
-        <TextInput
-          required
-          label="Asset Number"
-          {...form.getInputProps("assetNumber")}
-        />
-        <TextInput required label="Asset Number" />
-        <Button type="submit">Submit</Button>
-      </form>
+              <Divider my="xs" label="Hardware details" />
+              <Grid>
+                <Col span={6}>
+                  <Select
+                    label="Brand"
+                    required
+                    placeholder="Pick a brand"
+                    transition="pop-top-left"
+                    transitionDuration={80}
+                    transitionTimingFunction="ease"
+                    rightSection={<Loader size="xs" />}
+                    itemComponent={SelectItem}
+                    data={[
+                      {
+                        image:
+                          "https://img.icons8.com/clouds/256/000000/futurama-bender.png",
+                        label: "Bender Bending Rodríguez",
+                        value: "1",
+                        description: "Fascinated with cooking",
+                      },
+                    ]}
+                    searchable
+                    maxDropdownHeight={400}
+                    nothingFound="No data found"
+                    filter={(value, item) =>
+                      item.label
+                        .toLowerCase()
+                        .includes(value.toLowerCase().trim()) ||
+                      item.description
+                        .toLowerCase()
+                        .includes(value.toLowerCase().trim())
+                    }
+                  />
+                </Col>
+                <Col span={6}>
+                  <Select
+                    label="Modal"
+                    placeholder="Pick a model"
+                    itemComponent={SelectItem}
+                    data={[
+                      {
+                        image:
+                          "https://img.icons8.com/clouds/256/000000/futurama-bender.png",
+                        label: "Bender Bending Rodríguez",
+                        value: "1",
+                        description: "Fascinated with cooking",
+                      },
+                    ]}
+                    searchable
+                    maxDropdownHeight={400}
+                    nothingFound="No data found"
+                    filter={(value, item) =>
+                      item.label
+                        .toLowerCase()
+                        .includes(value.toLowerCase().trim()) ||
+                      item.description
+                        .toLowerCase()
+                        .includes(value.toLowerCase().trim())
+                    }
+                  />
+                </Col>
+                <Col span={4}>
+                  <TextInput label="Serial Number" />
+                </Col>
+                <Col span={4}>
+                  <TextInput label="Mac Address" />
+                </Col>
+              </Grid>
+
+              <Divider my="xs" label="Network details" />
+              <Group>
+                <TextInput label="IP Address" />
+                <TextInput label="Port" sx={{ width: "80px" }} />
+              </Group>
+              <Divider my="xs" variant="dashed" />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Card>
+        </div>
+      </Center>
     </section>
   );
 }
