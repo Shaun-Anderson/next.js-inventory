f6cd8463691660a534cc9a2023263f05ebe3b5b8
Shaun Anderson <shaun.anderson@outlook.com>
2021-12-27 15:14:38 +1100

    Added menu

--- pages/servers/index.tsx
+++ pages/servers/index.tsx
@@ -1,20 +1,23 @@
-import type { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import Layout from "../../components/layout";
 import {
   Button,
   ActionIcon,
   Grid,
-  Col,
+  Text,
   TextInput,
   Group,
   ThemeIcon,
+  Card,
+  Menu,
+  Divider,
 } from "@mantine/core";
 import { ReactTable } from "../../components/table";
 import { useModals } from "@mantine/modals";
 import { useForm, useForceUpdate } from "@mantine/hooks";
 import Link from "next/link";
 import Header from "../../components/header";
-import { Trash, Pencil, Plus, HardDrives } from "phosphor-react";
+import { Trash, Pencil, Plus, HardDrives, GitBranch } from "phosphor-react";
 import { useRouter } from "next/router";
 import Breadcrumbs from "../../components/breadcrumb";
 
@@ -25,16 +28,15 @@ type Server = {
   model: string;
   serial: string;
   macAddress: string;
+  ipAddress: string;
+  port: number;
+  location: string;
 };
 
 export default function About() {
   const router = useRouter();
 
   const columns = [
-    {
-      Header: "Id",
-      accessor: "id",
-    },
     {
       Header: "Asset Number",
       accessor: "assetNumber",
@@ -48,13 +50,38 @@ export default function About() {
       accessor: "model",
     },
     {
+      Header: "Network",
+      id: "networkLocation",
+      Cell: (data: any) => (
+        <Group spacing="5px" direction="column">
+          <Text size="sm">{data.row.original.ipAddress}</Text>
+          <Text size="xs" color="gray">
+            {data.row.original.port}
+          </Text>
+        </Group>
+      ),
+    },
+    {
       Header: "",
       id: "col13",
-      maxWidth: 50,
-      minWidth: 50,
-      width: 50,
+      maxWidth: 100,
+      minWidth: 100,
+      width: 100,
       Cell: () => (
         <Group spacing="5px">
+          <Menu
+            onClick={(e: any) => {
+              e.stopPropagation();
+            }}
+          >
+            <Menu.Label>Actions</Menu.Label>
+            <Menu.Item icon={<GitBranch />}>Change Status</Menu.Item>
+            <Menu.Item icon={<GitBranch />}>Move Locations</Menu.Item>
+            <Divider />
+            <Menu.Item color="red" icon={<Trash />}>
+              Delete
+            </Menu.Item>
+          </Menu>
           <ActionIcon
             color="yellow"
             variant="light"
@@ -76,7 +103,7 @@ export default function About() {
     },
   ];
 
-  const data: Server[] = [
+  const [data, setData] = useState<Server[]>([
     {
       id: 1,
       assetNumber: "Test #1",
@@ -84,6 +111,9 @@ export default function About() {
       model: "Test Model",
       serial: "00000001",
       macAddress: "1234",
+      ipAddress: "192.100.1.202",
+      port: 4000,
+      location: "Building B",
     },
     {
       id: 2,
@@ -92,6 +122,9 @@ export default function About() {
       model: "Test Model",
       serial: "00000001",
       macAddress: "1234",
+      ipAddress: "192.100.1.202",
+      port: 4000,
+      location: "Building B",
     },
     {
       id: 3,
@@ -100,56 +133,20 @@ export default function About() {
       model: "Test Model",
       serial: "00000001",
       macAddress: "1234",
+      ipAddress: "192.100.1.202",
+      port: 4000,
+      location: "Building B",
     },
-  ];
+  ]);
 
-  const modals = useModals();
-  const add = () => {
-    console.log("Add");
-  };
-  const form = useForm<Server>({
-    initialValues: {
-      id: 0,
-      assetNumber: "",
-      brand: "",
-      model: "",
-      serial: "",
-      macAddress: "",
-    },
-    validationRules: {
-      assetNumber: (value) => /^\S+@\S+$/.test(value),
-    },
-  });
-  const forceUpdate = useForceUpdate();
-
-  const openAddModal = () => {
-    const add = () => {
-      console.log("Add");
-      modals.closeModal(id);
-    };
-
-    const id = modals.openModal({
-      title: "Add Server",
-      children: (
-        <form onSubmit={form.onSubmit((values) => add())}>
-          <TextInput
-            required
-            label="Asset Number"
-            onChange={() => {
-              form.getInputProps("assetNumber").onChange();
-              forceUpdate();
-            }}
-            error={form.getInputProps("assetNumber").error}
-          />
-          <TextInput required label="Asset Number" />
-          <Button type="submit">Submit</Button>
-        </form>
-      ),
-    });
+  const add = (newServer: Server) => {
+    setData([...data, newServer]);
   };
 
   return (
-    <section>
+    <section
+      style={{ height: "100%", display: "flex", flexDirection: "column" }}
+    >
       <Header
         icon={
           <ThemeIcon size="lg" color="pink" variant="light">
@@ -171,14 +168,18 @@ export default function About() {
           </Link>
         }
       />
-      <ReactTable<Server>
-        data={data}
-        columns={columns}
-        onRowClick={(row: Server) => {
-          console.log(row);
-          router.push(`/servers/${row.id}`);
-        }}
-      />
+      <Card>
+        <ReactTable<Server>
+          data={data}
+          searchable
+          selectable
+          columns={columns}
+          onRowClick={(row: Server) => {
+            console.log(row);
+            router.push(`/servers/${row.id}`);
+          }}
+        />
+      </Card>
     </section>
   );
 }
